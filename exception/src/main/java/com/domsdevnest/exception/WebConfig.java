4ef0a7fb70fbad90bcf7eb212e6da2663e22e3bf
domsdevnest <domsdevnest@gmail.com>
2025-03-10 21:54:09 +0900

    HandlerExceptionResolver를 이용한 예외처리 기본

--- exception/src/main/java/com/domsdevnest/exception/WebConfig.java
+++ exception/src/main/java/com/domsdevnest/exception/WebConfig.java
@@ -2,13 +2,17 @@ package com.domsdevnest.exception;
 
 import com.domsdevnest.exception.filter.LogFilter;
 import com.domsdevnest.exception.interceptor.LogInterceptor;
+import com.domsdevnest.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -24,7 +28,12 @@ public class WebConfig implements WebMvcConfigurer {
                 );
     }
 
-//    @Bean
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -33,4 +42,6 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
         return filterRegistrationBean;
     }
+
+
 }
--- exception/src/main/java/com/domsdevnest/exception/WebServerCustomizer.java
+++ exception/src/main/java/com/domsdevnest/exception/WebServerCustomizer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 //@Component
 public class WebServerCustomizer implements
