5baf5ce6f3ae0635d5dd640bb15809893c0d9d4f
domsdevnest <domsdevnest@gmail.com>
2025-03-12 22:37:24 +0900

    @ControllerAdvice , @RestControllerAdvice 를 사용한 예외처리

---
+++ exception/src/main/java/com/domsdevnest/exception/advice/ExControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.domsdevnest.exception.advice;
+
+import com.domsdevnest.exception.exception.UserException;
+import com.domsdevnest.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.domsdevnest.exception.api")
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER_EX", e.getMessage());
+        return new ResponseEntity(errorResult,HttpStatus.BAD_REQUEST);
+    }
+
+    // IllegalArgumentException , UserException 이 아닌 모든 예외 처리
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부오류");
+    }
+
+}
--- exception/src/main/java/com/domsdevnest/exception/api/ApiExceptionV3Controller.java
+++ exception/src/main/java/com/domsdevnest/exception/api/ApiExceptionV3Controller.java
@@ -1,19 +1,18 @@
 package com.domsdevnest.exception.api;
 
 import com.domsdevnest.exception.exception.UserException;
-import com.domsdevnest.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class ApiExceptionV2Controller {
+public class ApiExceptionV3Controller {
 
-    @GetMapping("/api2/members/{id}")
+    @GetMapping("/api3/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
 
         if (id.equals("ex")) {
