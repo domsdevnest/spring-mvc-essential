5e1f054f1b5bd19a169b2f35b584e6aba22394f3
domsdevnest <domsdevnest@gmail.com>
2025-03-13 22:31:46 +0900

    타입 변환 #1

---
+++ typeconverter/src/main/java/com/domsdevnest/typeconverter/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.domsdevnest.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    /**
+     * 타입 직접 변환
+     */
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam을 통해 변환
+     */
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
+
+
